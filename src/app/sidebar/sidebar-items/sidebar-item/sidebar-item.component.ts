2c7c5e51c1681ce87b801c6bdbc9268a75434ef5
Papa Abdou Mbodji <abdoumbodji1401@gmail.com>
2025-01-13 14:25:36 +0100

    Initial commit

---
+++ src/app/sidebar/sidebar-items/sidebar-item/sidebar-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './sidebar-item.component.html',
+  styleUrl: './sidebar-item.component.css'
+})
+export class SidebarItemComponent {
+
+}
---
+++ src/app/sidebar/sidebar-items/sidebar-items.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-items',
+  standalone: true,
+  imports: [],
+  templateUrl: './sidebar-items.component.html',
+  styleUrl: './sidebar-items.component.css'
+})
+export class SidebarItemsComponent {
+
+}
