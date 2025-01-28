c5a90d26c05cfa92e352b1a2a1d896d7cde6f5ed
Mady95 <ms.sambake@gmail.com>
2025-01-28 14:25:18 +0100

    Front dashboard

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-sidebar',
-  standalone: true,
-  imports: [],
-  templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  selector: 'app-sidebar', // Le sélecteur du composant
+  standalone: true, // Rend le composant standalone
+  imports: [CommonModule], // Modules nécessaires pour ce composant
+  templateUrl: './sidebar.component.html', // Lien vers le template
+  styleUrls: ['./sidebar.component.css'] // Lien vers les styles
 })
-export class SidebarComponent {
-
-}
+export class SidebarComponent { }
