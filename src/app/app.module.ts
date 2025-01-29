ae4483cd83e6b5af1b268b0526d59417f53ef542
Sab Ben <sbenseghir.sb@gmail.com>
2025-01-29 17:11:04 +0100

    Mise en place du routing pour les opérations bancaires, reformatage du code et nettoyage du code inutilisé

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { BannerComponent } from './banner/banner.component';
-import { CalendarComponent } from './dashboard/calendar/calendar.component'; // Import du composant Calendar
-import { SummaryComponent } from './dashboard/summary/summary.component'; // Import du composant Summary
+
+bootstrapApplication(AppComponent);
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SidebarComponent,
-    BannerComponent,
-    CalendarComponent, // Ajoute le composant Calendar
-    SummaryComponent // Ajoute le composant Summary
+
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  
 })
+
 export class AppModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { BankOperationsComponent } from './bank-operations/bank-operations.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: DashboardComponent},
+    { path: 'bank-operations-component', component: BankOperationsComponent },
+];
