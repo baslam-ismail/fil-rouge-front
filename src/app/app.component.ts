184e7da0501d8e154e18faccf684d93db1707024
Sab Ben <sbenseghir.sb@gmail.com>
2025-01-31 12:49:48 +0100

    Ajout du widget calendar avec la libraire Tailwind CSS et l'UI Flowbite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { initFlowbite } from 'flowbite';
 
 
 @Component({
@@ -14,4 +14,9 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'front-malsi';
+
+  ngOnInit(): void {
+      initFlowbite();
+
+  }
 }
