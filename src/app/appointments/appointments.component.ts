a81a12d070f3506ba2dfb6efa5ccbac19d1dd114
Mady95 <ms.sambake@gmail.com>
2025-01-31 16:14:02 +0100

    Many update of forms to create account and client

--- src/app/appointments/appointments.component.ts
+++ src/app/appointments/appointments.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { BannerComponent } from '../banner/banner.component';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 
@@ -7,11 +9,61 @@ import { SidebarComponent } from '../sidebar/sidebar.component';
   standalone: true,
   imports: [
     SidebarComponent,
-    BannerComponent 
+    BannerComponent,
+    CommonModule,
+    FormsModule
   ],
   templateUrl: './appointments.component.html',
-  styleUrl: './appointments.component.css'
+  styleUrls: ['./appointments.component.css']
 })
-export class AppointmentsComponent {
+export class AppointmentsComponent implements OnInit {
+  appointments = [
+    { motif: 'Ouverture compte', clientName: 'Alain FRENAULT', clientNumber: '06 09 49 66 43', priority: 'Haute', date: new Date('2024-06-07'), status: 'En attente' },
+    { motif: 'Assurance vie', clientName: 'Sofia ARLOUCHE', clientNumber: '06 49 49 69 43', priority: 'Haute', date: new Date('2024-06-04'), status: 'Confirmé' },
+    { motif: 'Demande prêt', clientName: 'Mamadou TOURE', clientNumber: '06 29 19 66 43', priority: 'Moyenne', date: new Date('2024-06-03'), status: 'Annulé' },
+    { motif: 'Changement d’adresse', clientName: 'Iga SWIATEK', clientNumber: '06 49 42 64 73', priority: 'Basse', date: new Date('2024-05-28'), status: 'Confirmé' },
+    { motif: 'Assurance habitation', clientName: 'Maria SHARAPOVA', clientNumber: '07 09 49 67 43', priority: 'Moyenne', date: new Date('2024-05-20'), status: 'En attente' }
+  ];
 
+  itemsPerPage = 2;
+  currentPage = 1;
+  totalPages = 0;
+
+  ngOnInit(): void {
+    this.calculatePages();
+  }
+
+  get paginatedAppointments() {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    return this.appointments.slice(startIndex, endIndex);
+  }
+
+  get displayStart(): number {
+    return (this.currentPage - 1) * this.itemsPerPage + 1;
+  }
+
+  get displayEnd(): number {
+    return Math.min(this.currentPage * this.itemsPerPage, this.appointments.length);
+  }
+
+  calculatePages(): void {
+    this.totalPages = Math.ceil(this.appointments.length / this.itemsPerPage);
+  }
+
+  prevPage(): void {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+    }
+  }
+
+  nextPage(): void {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+    }
+  }
+
+  goToPage(page: number): void {
+    this.currentPage = page;
+  }
 }
