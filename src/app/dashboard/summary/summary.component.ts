52a58209ec6830f0f79cd58c080236e6c489b231
LiticiaAmn <amaniliticia30@gmail.com>
2025-02-16 20:14:48 +0100

    Maj

--- src/app/dashboard/summary/summary.component.ts
+++ src/app/dashboard/summary/summary.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CalendarComponent } from "../calendar/calendar.component";
-import { SidebarComponent } from "../../sidebar/sidebar.component";
-import { BannerComponent } from "../../banner/banner.component";
-import { ChatbotComponent } from "../../chatbot/chatbot.component";
 
 @Component({
   selector: 'app-summary',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, CalendarComponent, SidebarComponent, BannerComponent, ChatbotComponent],
+  imports: [CalendarComponent,RouterLink, RouterLinkActive],
   templateUrl: './summary.component.html',
   styleUrl: './summary.component.css'
 })
